b4928ef8301b8eedb51e8d48df1f5573d8c2f2e6
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-06-16 11:14:09 -0700

    Added backtracking algorithm.

--- src/main/java/dynamicprograming/OneExpression/OneExpression.java
+++ src/main/java/dynamicprograming/OneExpression/OneExpression.java
@@ -1,6 +1,9 @@
 package dynamicprograming.OneExpression;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
@@ -43,36 +46,28 @@ public class OneExpression {
             copyArrayValues(m);
         }
 
-        // TODO implement algorithm to compute matrix values.
-
         if (dpMatrix[m] == 0 && m > 0) {
 
             // Check case where m = x * y
             int currentMinNumOfOnesMultiplication = m;
-            int multiplicationIndex;
 
             // iterate over all divisors of m, upto root m.
             for(int i=2; i<Math.ceil(Math.sqrt(m)); i++) {
                 if (m % i == 0) {
-
                     int newMinNumOfOnes = computeRecurrence(m / i) + computeRecurrence(i);
                     if (newMinNumOfOnes < currentMinNumOfOnesMultiplication) {
-                        multiplicationIndex = i;
                         currentMinNumOfOnesMultiplication = newMinNumOfOnes;
                     }
-
                 }
             }
 
             // Check case where m = x + y
             int currentMinNumOfOnesAddition = m;
-            int additionIndex;
 
             // iterate over all subtractions, up to m/2
             for(int i=1; i<Math.ceil(m / 2.0); i++) {
                 int newMinNumOfOnes = computeRecurrence(m - i) + computeRecurrence(i);
                 if(newMinNumOfOnes < currentMinNumOfOnesAddition) {
-                    additionIndex = i;
                     currentMinNumOfOnesAddition = newMinNumOfOnes;
                 }
             }
@@ -89,12 +84,72 @@ public class OneExpression {
 
     // Use values stored in dpMatrix to compute expression for m.
     public String computeExpression(int m) {
+        // Ensure dpMatrix is populated.
         if (dpMatrix.length > m + 1) {
             computeRecurrence(m);
         }
 
-        // TODO implement
-        return null;
+        StringBuilder send = new StringBuilder();
+
+        if (m == 1) {
+            return "1";
+        } else {
+            // Check case where m = x * y
+            int currentMinNumOfOnesMultiplication = m;
+            int multiplicationIndex = 1;
+
+            // iterate over all divisors of m, upto root m.
+            for(int i=2; i<Math.ceil(Math.sqrt(m)); i++) {
+                if (m % i == 0) {
+
+                    int newMinNumOfOnes = computeRecurrence(m / i) + computeRecurrence(i);
+                    if (newMinNumOfOnes < currentMinNumOfOnesMultiplication) {
+                        multiplicationIndex = i;
+                        currentMinNumOfOnesMultiplication = newMinNumOfOnes;
+                    }
+                }
+            }
+
+            // Check case where m = x + y
+            int currentMinNumOfOnesAddition = m;
+            int additionIndex = 1;
+
+            // iterate over all subtractions, up to m/2
+            for(int i=1; i<Math.ceil(m / 2.0); i++) {
+                int newMinNumOfOnes = computeRecurrence(m - i) + computeRecurrence(i);
+                if(newMinNumOfOnes < currentMinNumOfOnesAddition) {
+                    additionIndex = i;
+                    currentMinNumOfOnesAddition = newMinNumOfOnes;
+                }
+            }
+
+            // Compare the two cases, adding to string appropriately.
+            if(currentMinNumOfOnesMultiplication < currentMinNumOfOnesAddition) {
+                send.append("("+computeExpression(m / multiplicationIndex)+")("+computeExpression(multiplicationIndex)+")");
+            } else {
+                send.append(computeExpression(m - additionIndex) + "+" + computeExpression(additionIndex));
+            }
+        }
+        return send.toString();
+    }
+
+    // Print dpMatrix in csv format
+    public void printMatrixToFile(String filepath) {
+
+        try(PrintWriter writer = new PrintWriter(new File(filepath))) {
+
+            StringBuilder builder = new StringBuilder();
+
+            for(int i=1; i<dpMatrix.length; i++) {
+                builder.append(i).append(",").append(dpMatrix[i]);
+                builder.append("\n");
+            }
+
+            writer.write(builder.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private void copyArrayValues(int m) {
@@ -108,9 +163,14 @@ public class OneExpression {
 
     public static void main(String[] args) {
         OneExpression o = new OneExpression();
-        o.computeRecurrence(50);
-        //Testing
-        System.out.println(o.dpMatrix.length);
-        System.out.println(Arrays.toString(o.dpMatrix));
+        // Testing
+        int m = 12;
+
+        o.computeRecurrence(m);
+        System.out.println(o.dpMatrix[m]);
+//        o.printMatrixToFile(m+".csv");
+        System.out.println(o.computeExpression(m));
+        System.out.println(o.dpMatrix[6]);
+        System.out.println(o.computeExpression(6));
     }
 }
